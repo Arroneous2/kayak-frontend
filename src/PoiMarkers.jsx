ad539d72588c0628c5e8cb52be079271dcec0677
Arroneous2 <thumpinbassbeat@gmail.com>
2024-07-16 13:00:59 -0500

    show position of places for trip

--- src/PoiMarkers.jsx
+++ src/PoiMarkers.jsx
@@ -1,10 +1,17 @@
 import { AdvancedMarker, Pin } from "@vis.gl/react-google-maps";
 
 export function PoiMarkers(props) {
+  console.log("props of poi", props.places[0].lat);
+
   return (
     <div>
-      {props.pois.map((poi) => (
-        <AdvancedMarker key={poi.key} position={poi.location} clickable={true} draggable={true}>
+      {props.places.map((place) => (
+        <AdvancedMarker
+          key={place.id}
+          position={{ lat: parseFloat(place.lat), lng: parseFloat(place.lng) }}
+          clickable={true}
+          draggable={true}
+        >
           <Pin background={"#FBBC04"} glyphColor={"#000"} borderColor={"#000"} />
         </AdvancedMarker>
       ))}
